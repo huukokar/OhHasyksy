e2aa7c909570964c90fc1a221c6243851e24bfcd
Huuko Kareinen <huuko.kareinen@helsinki.fi>
2013-09-19 21:55:00 +0300

    paivitys testiin vol 2

---
+++ BlackJack/src/blackjack/KortinArvo.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/**
+ *
+ * @author huukokar
+ */
+public enum KortinArvo {
+    
+}
---
+++ BlackJack/test/Luokkientestausta.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Huksi
+ */
+public class Luokkientestausta {
+    import blackjack.BlackJack;
+    import blackjack.Kortti;
+    import blackjack.Korttipakka;
+    import blackjack.Korttipakkakokoelma;
+    
+    public Luokkientestausta() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() {
+         Korttipakkakokoelma testikokoelma = new Korttipakkakokoelma("Testaan vain");
+         Korttipakka testipakka = new Korttipakka(testikokoelma, "tuPakka");
+         Kortti testikortti = new Kortti(testipakka, 1, 5);
+         assertEquals("Kortti on patavitonen, ja sen omistaja on tuPakka", testikortti.toString());
+                 
+     }
+}
